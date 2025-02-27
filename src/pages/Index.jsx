93454e58248fedeeb119a8320857623f6020717e
MD ISMAIL <mdismail85773@gmail.com>
2025-02-27 19:24:45 +0800

    brand/newproduct/newsletter/recentlyviewproduct/footer

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -5,16 +5,27 @@ import ProductsSlider from '../pertials/ProductsSlider';
 import Policy from '../pertials/Policy';   
 import Products from '../pertials/Products';
 import Promotion from '../pertials/Promotion';
+import NewArrivalProducts from '../pertials/NewArrivalProducts';
+import BrandSection from '../pertials/BrandSection';
+import RecentlyViewProducts from '../pertials/RecentlyViewProducts';
+import NewsLetter from '../pertials/NewsLetter';
+import Footer from '../pertials/Footer';
 
 const Index = () => {
     return (
         <div>
-            <Topheader />
-            <Navbar />
-            <ProductsSlider />
-            <Policy />
+            <Topheader/>
+            <Navbar/>
+            <ProductsSlider/>
+            <Policy/>
             <Products/>
             <Promotion/>
+            <NewArrivalProducts/>
+            <BrandSection/>
+            <RecentlyViewProducts/>
+            <NewsLetter/>
+            <Footer/>
+
         </div>
     );
 };
