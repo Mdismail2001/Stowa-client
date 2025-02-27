4f2fd001600c4f4ace06605d9f9c4dcfb889aeb3
MD ISMAIL <mdismail85773@gmail.com>
2025-02-27 16:19:47 +0800

    policy/product/slider/promotion sections include

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import Topheader from '../pertials/Topheader';
 import Navbar from '../pertials/Navbar';
+import ProductsSlider from '../pertials/ProductsSlider'; 
+import Policy from '../pertials/Policy';   
+import Products from '../pertials/Products';
+import Promotion from '../pertials/Promotion';
 
 const Index = () => {
     return (
         <div>
             <Topheader />
             <Navbar />
+            <ProductsSlider />
+            <Policy />
+            <Products/>
+            <Promotion/>
         </div>
     );
 };
